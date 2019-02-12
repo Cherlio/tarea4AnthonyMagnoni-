1403bcfc2d1325e4c69e67f904d0d2ef489f5caf
Cherlio <cherlio69@gmail.com>
2019-02-12 14:21:51 +0100

    Añade atributos en clase modelo

---
+++ src/tarea4anthonymagnoni/Coche.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4anthonymagnoni;
+
+/**
+ *
+ * @author anthony
+ */
+public class Coche {
+    
+    private int puertas;
+    private int rueda;
+    private boolean sport;
+    
+}
